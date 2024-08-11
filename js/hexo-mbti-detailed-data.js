bab30c98e25d6000669d53149f62252b47349551
baobaodz <3208417157@qq.com>
2024-08-11 23:04:07 +0800

    Site updated: 2024-08-11 23:04:05

---
+++ js/hexo-mbti-detailed-data.js
@@ -0,0 +1,177 @@
+const personalityTypes = [{
+        type: 'INTJ',
+        name: {
+            zh: '建筑师',
+            en: 'Architect'
+        },
+        desc: {
+            zh: '富有想象力和战略性思维，一切皆在计划之中。',
+            en: 'Architects are imaginative and strategic thinkers, with a plan for everything.'
+        },
+    },
+    {
+        type: 'INTP',
+        name: {
+            zh: '逻辑学家',
+            en: 'Logician'
+        },
+        desc: {
+            zh: '具有创造力的发明家，对知识有着不可抑制的渴望。',
+            en: 'Innovative inventors with an unquenchable thirst for knowledge.'
+        },
+    },
+    {
+        type: 'ENTJ',
+        name: {
+            zh: '指挥官',
+            en: 'Commander'
+        },
+        desc: {
+            zh: '大胆、富有想象力且意志强大的领导者，总能找到或创造解决方法。',
+            en: 'Bold, imaginative and strong-willed leaders, always finding a way – or making one.'
+        },
+    },
+    {
+        type: 'ENTP',
+        name: {
+            zh: '辩论家',
+            en: 'Debater'
+        },
+        desc: {
+            zh: '聪明好奇的思考者，无法抵挡智力挑战的诱惑。',
+            en: 'Smart and curious thinkers who cannot resist an intellectual challenge.'
+        },
+    },
+    {
+        type: 'INFJ',
+        name: {
+            zh: '提倡者',
+            en: 'Advocate'
+        },
+        desc: {
+            zh: '安静神秘，但非常鼓舞人心且不知疲倦的理想主义者。',
+            en: 'Quiet and mystical, yet very inspiring and tireless idealists.'
+        },
+    },
+    {
+        type: 'INFP',
+        name: {
+            zh: '调停者',
+            en: 'Mediator'
+        },
+        desc: {
+            zh: '富有诗意、善良且无私的人，总是热衷于帮助正义事业。',
+            en: 'Logisticians are practical and fact-minded individuals, whose reliability cannot be doubted.'
+        },
+    },
+    {
+        type: 'ENFJ',
+        name: {
+            zh: '主人公',
+            en: 'Protagonist'
+        },
+        desc: {
+            zh: '具有魅力并能激励人心的领导者，能够让听众为之着迷。',
+            en: 'Charismatic and inspiring leaders, able to mesmerize their listeners.'
+        },
+    },
+    {
+        type: 'ENFP',
+        name: {
+            zh: '竞选者',
+            en: 'Campaigner'
+        },
+        desc: {
+            zh: '充满活力，富有创意，善于交际的自由之人，总能找到微笑的理由。',
+            en: 'Enthusiastic, creative and sociable free spirits, who can always find a reason to smile.'
+        },
+    },
+    {
+        type: 'ISTJ',
+        name: {
+            zh: '物流师',
+            en: 'Logistician'
+        },
+        desc: {
+            zh: '务实且注重事实的人，可靠性不容怀疑。',
+            en: 'Logisticians are practical and fact-minded individuals, whose reliability cannot be doubted.'
+        },
+    },
+    {
+        type: 'ISFJ',
+        name: {
+            zh: '守卫者',
+            en: 'Defender'
+        },
+        desc: {
+            zh: '非常专注和热情的保护者，总是随时准备保护他们所爱的人。',
+            en: 'Very dedicated and warm protectors, always ready to defend their loved ones.'
+        },
+    },
+    {
+        type: 'ESTJ',
+        name: {
+            zh: '总经理',
+            en: 'Executive'
+        },
+        desc: {
+            zh: '出色的管理者，在管理事物或人的方面无与伦比。',
+            en: 'Excellent administrators, unsurpassed at managing things – or people.'
+        },
+    },
+    {
+        type: 'ESFJ',
+        name: {
+            zh: '执政官',
+            en: 'Consul'
+        },
+        desc: {
+            zh: '非常关心他人，善于社交，受人欢迎，总是乐于助人。',
+            en: 'Extraordinarily caring, social and popular people, always eager to help.'
+        },
+    },
+    {
+        type: 'ISTP',
+        name: {
+            zh: '鉴赏家',
+            en: 'Virtuoso'
+        },
+        desc: {
+            zh: '大胆而实际的实验家，擅长使用各种形式的工具。',
+            en: 'Bold and practical experimenters, masters of all kinds of tools.'
+        },
+    },
+    {
+        type: 'ISFP',
+        name: {
+            zh: '探险家',
+            en: 'Adventurer'
+        },
+        desc: {
+            zh: '灵活有魅力的艺术家，时刻准备着探索和体验新鲜事物。',
+            en: 'Flexible and charming artists, always ready to explore and experience something new.'
+        },
+    },
+    {
+        type: 'ESTP',
+        name: {
+            zh: '企业家',
+            en: 'Entrepreneur'
+        },
+        desc: {
+            zh: '聪明、充满活力且洞察力极强的人，真正喜欢充满刺激和危险的生活。',
+            en: 'Smart, energetic and very perceptive people, who truly enjoy living on the edge.'
+        },
+    },
+    {
+        type: 'ESFP',
+        name: {
+            zh: '表演者',
+            en: 'Entertainer'
+        },
+        desc: {
+            zh: '精力充沛、热情，总是心血来潮--有他们在身边，生活永远不会无聊。',
+            en: 'Spontaneous, energetic and enthusiastic people – life is never boring around them.'
+        },
+    },
+];
---
+++ js/hexo-mbti-detailed.js
@@ -0,0 +1,309 @@
+function initializeDetailedMBTI(config) {
+    console.log('config: ', config)
+    const imagesHostUrl = 'https://cdn.jsdelivr.net/gh/baobaodz/picx-images-hosting@master/hexo-mbti'
+    const createHeaderHTML = (personalityType) => {
+        const resetButton = config.slide ?
+            `
+            <div id="reset-mbti" class="reset-button">
+                <svg t="1722666313154" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="28600" width="32" height="32" style="padding: 6px;">
+                    <path d="M307.045 256.85l112.95 30.285-21.51 80.234-160.559-43.02-80.325-21.465L222.176 62l80.28 21.51-23.306 87.075C421.44 57.412 624.506 62.952 760.42 183.715s165.298 321.766 69.649 476.387-288.631 218.062-457.367 150.364-264.36-246.93-226.62-424.781l80.46 21.598a290.925 290.925 0 1 0 80.55-150.385l-0.046-0.045zM137.981 878.883h748.125V962H137.98v-83.117z" fill="#6e6e6e" fill-opacity=".8" p-id="28601"></path>
+                </svg>            
+            </div>
+            ` : '';
+        const downloadButton = `
+            <div id="download-mbti">
+                <svg t="1722664481467" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1500" width="32" height="32">
+                    <path d="M276.195556 325.688889v244.195555l149.959111-106.126222a28.444444 28.444444 0 0 1 33.735111 0.625778l77.966222 59.648 164.721778-130.474667V325.688889H276.195556z m0 313.884444v58.737778h280.177777a28.444444 28.444444 0 1 1 0 56.888889H261.973333c-23.665778 0-42.666667-19.000889-42.666666-42.666667v-401.066666c0-23.665778 19.000889-42.666667 42.666666-42.666667H716.8c23.665778 0 42.666667 19.000889 42.666667 42.666667v95.857777a28.444444 28.444444 0 0 1-10.808889 22.300445l-192.853334 152.746667a28.444444 28.444444 0 0 1-34.901333 0.284444l-78.961778-60.387556-165.745777 117.276445z" fill="#6e6e6e" fill-opacity=".8" p-id="1501"></path>
+                    <path d="M364.088889 438.328889a35.555556 35.555556 0 1 0 0-71.111111 35.555556 35.555556 0 0 0 0 71.111111z" fill="#6e6e6e" fill-opacity=".8" p-id="1502"></path>
+                    <path d="M813.425778 630.698667a28.444444 28.444444 0 0 1 0 40.220444l-75.946667 75.946667a28.444444 28.444444 0 0 1-40.220444 0l-75.662223-75.662222a28.444444 28.444444 0 0 1 40.220445-40.220445l55.552 55.552 55.836444-55.836444a28.444444 28.444444 0 0 1 40.220445 0z" fill="#000000" fill-opacity=".8" p-id="1503"></path><path d="M717.368889 521.102222a28.444444 28.444444 0 0 1 28.444444 28.444445v176.924444a28.444444 28.444444 0 1 1-56.888889 0v-176.924444a28.444444 28.444444 0 0 1 28.444445-28.444445z" fill="#6e6e6e" fill-opacity=".8" p-id="1504"></path>
+                </svg>
+            </div>
+            `
+
+        return `
+            <div class="mbti-header">
+                <div class="personality-avatar"> 
+                    <img src="${personalityType.avatar}" alt="${personalityType.name}">
+                </div>
+                <div class="personality-info">
+                    <div class="personality-name">${personalityType.name} (${personalityType.type})</div>
+                    <div class="personality-desc">${personalityType.desc}</div>
+                </div>
+                <div class="personality-btns">
+                    ${resetButton}
+                    ${downloadButton}
+                </div>
+ 
+            </div>
+        `;
+    }
+
+    const createBarHTML = (dimension) => {
+        return `
+            <div class="trait-container">
+                <span class="trait-left">${dimension.label[0]}</span>
+                <div class="slider-container">
+                    <input type="range" min="0" max="100" value="${dimension.value[1]}" class="slider" id="${dimension.id}Slider" disabled>
+                    <div id="${dimension.id}SliderTooltip" class="slider-tooltip">
+                        <span class="trait-value"></span>
+                        <span class="percentage-value">%</span>
+                    </div>
+                </div>
+                <span class="trait-right">${dimension.label[1]}</span>
+            </div>
+        `;
+    }
+    const createFooterHTML = (personalityType) => {
+        const langConfig = {
+            prefix: { zh: 'ch/', en: '' },
+            suffix: { zh: '人格', en: 'personality' },
+            linkText: { zh: '查看性格剖析', en: 'View Detailed Personality Analysis' }
+        };
+
+        const baseUrl = 'https://www.16personalities.com/';
+        const lang = config.language || 'en';
+        const url = `${baseUrl}${langConfig.prefix[lang]}${personalityType.type.slice(0,4).toLowerCase()}-${langConfig.suffix[lang]}`;
+
+        return `
+            <div class="mbti-footer">
+                <span class="mbti-personality-link ">
+                    <a href="${url}" target="_blank">${langConfig.linkText[lang]}</a>
+                </span>
+            </div>
+        `;
+    }
+    const languageConfig = {
+        en: {
+            ei: ['Extraverted', 'Introverted'],
+            ns: ['Intuitive', 'Observant'],
+            tf: ['Thinking', 'Feeling'],
+            jp: ['Judging', 'Prospecting'],
+            at: ['Assertive', 'Turbulent']
+        },
+        zh: {
+            ei: ['外向', '内向'],
+            ns: ['有远见', '现实'],
+            tf: ['理性分析', '感受'],
+            jp: ['评判', '展望'],
+            at: ['坚决', '起伏不定']
+        }
+    };
+
+    const getMBTIDimensions = (config) => {
+        const lang = config.language || 'en';
+        return [
+            { id: 'ei', label: languageConfig[lang].ei, value: config.data['E-I'] || config.data[languageConfig[lang].ei.join('-')], color: config.color[0] },
+            { id: 'ns', label: languageConfig[lang].ns, value: config.data['N-S'] || config.data[languageConfig[lang].ns.join('-')], color: config.color[1] },
+            { id: 'tf', label: languageConfig[lang].tf, value: config.data['T-F'] || config.data[languageConfig[lang].tf.join('-')], color: config.color[2] },
+            { id: 'jp', label: languageConfig[lang].jp, value: config.data['J-P'] || config.data[languageConfig[lang].jp.join('-')], color: config.color[3] },
+            { id: 'at', label: languageConfig[lang].at, value: config.data['A-T'] || config.data[languageConfig[lang].at.join('-')], color: config.color[4] },
+        ];
+    }
+
+
+    const getMBTIBars = (dimensions) => {
+        return dimensions.map(createBarHTML).join('');
+    }
+
+    const updateSliderDisplay = (value, dimension, slider, sliderTooltip) => {
+        const index = getDominantTraitIndex(value);
+        const trait = dimension.label[index];
+        const percentage = index ? value : 100 - value;
+        sliderTooltip.querySelector('.trait-value').textContent = trait;
+        sliderTooltip.querySelector('.percentage-value').textContent = `${percentage}%`;
+        const position = (value / 100) * slider.offsetWidth;
+        sliderTooltip.style.left = `${position}px`;
+    }
+
+    const initializeSliders = (dimensions, isSlideEnabled) => {
+        dimensions.forEach(dimension => {
+            const slider = document.getElementById(`${dimension.id}Slider`);
+            const sliderTooltip = document.getElementById(`${dimension.id}SliderTooltip`);
+
+            updateSliderDisplay(slider.value, dimension, slider, sliderTooltip);
+            applySliderStyles(slider, sliderTooltip, dimension.color);
+
+            if (isSlideEnabled) {
+                enableSliderInteraction(slider, dimension, sliderTooltip);
+            }
+        });
+    }
+
+    const applySliderStyles = (slider, sliderTooltip, color) => {
+        slider.style.setProperty('--slider-color', color);
+        if (config.tooltip && config.tooltip.showTooltipBackground) {
+            sliderTooltip.style.setProperty('--slider-color', color);
+        } else {
+            sliderTooltip.style.setProperty('--slider-color', 'transparent');
+            sliderTooltip.style.top = '-26px';
+            sliderTooltip.querySelector('.trait-value').style.color = color;
+            sliderTooltip.querySelector('.percentage-value').style.color = '#000';
+            sliderTooltip.classList.add('no-after');
+
+
+        }
+    }
+
+    const enableSliderInteraction = (slider, dimension, sliderTooltip) => {
+        slider.removeAttribute('disabled');
+        let lastType = getDominantTraitIndex(dimension.value[0]);
+        const debouncedUpdateSliderDisplay = debounce(updateSliderDisplay, 100);
+        slider.addEventListener('input', (e) => {
+            debouncedUpdateSliderDisplay(e.target.value, dimension, slider, sliderTooltip);
+            const index = mbtiDimensions.findIndex(d => d.id === dimension.id);
+            const sliderValue = parseInt(e.target.value);
+
+            mbtiDimensions[index].value[0] = 100 - sliderValue;
+            mbtiDimensions[index].value[1] = sliderValue;
+
+            const currentType = getDominantTraitIndex(mbtiDimensions[index].value[0]);
+            if (currentType !== lastType) {
+                lastType = currentType;
+                currentPersonalityType = calculatePersonalityType(mbtiDimensions);
+                updatePersonalityTypeDisplay(currentPersonalityType);
+                updatePersonalityLink(currentPersonalityType);
+            }
+        });
+    }
+    const updateAllTooltipPositions = () => {
+        mbtiDimensions.forEach(dimension => {
+            const slider = document.getElementById(`${dimension.id}Slider`);
+            const sliderTooltip = document.getElementById(`${dimension.id}SliderTooltip`);
+            updateSliderDisplay(slider.value, dimension, slider, sliderTooltip);
+        });
+    }
+    const updatePersonalityTypeDisplay = (personalityType) => {
+        const headerElement = document.querySelector('.mbti-header');
+        const personalityInfoElement = headerElement.querySelector('.personality-info');
+        const avatarElement = headerElement.querySelector('.personality-avatar img');
+
+        // 更新头像
+        avatarElement.src = personalityType.avatar;
+        avatarElement.alt = personalityType.name;
+
+        // 更新个性类型信息
+        personalityInfoElement.innerHTML = `
+            <div class="personality-name">${personalityType.name} (${personalityType.type})</div>
+            <div class="personality-desc">${personalityType.desc}</div>
+        `;
+
+    }
+
+    function updatePersonalityLink(personalityType) {
+        const link = document.querySelector('.mbti-personality-link a');
+        link.href = link.href.replace(/\/[a-z]{4}-/, `/${personalityType.type.slice(0,4).toLowerCase()}-`);
+    }
+    const getDominantTraitIndex = (value) => {
+        return value > 50 ? 1 : 0;
+    }
+    const getLocalizedContent = (content) => {
+        return content[config.language];
+    }
+
+    const resetMBTI = () => {
+        console.log('Resetting MBTI baseMBTIDimensions: ', baseMBTIDimensions);
+        baseMBTIDimensions.forEach((dimension, index) => {
+            const slider = document.getElementById(`${dimension.id}Slider`);
+            const sliderTooltip = document.getElementById(`${dimension.id}SliderTooltip`);
+            const initialValue = dimension.value[1];
+            slider.value = initialValue;
+            updateSliderDisplay(initialValue, dimension, slider, sliderTooltip);
+        });
+        updatePersonalityTypeDisplay(basePersonalityType);
+    }
+    const generatePersonalityImage = () => {
+        const filterFn = (node) => {
+            if (node.classList) {
+                if (node.classList.contains('personality-btns') || node.classList.contains('mbti-footer')) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        const fetchOptions = {
+            requestInit: {
+                mode: 'cors',
+            },
+            bypassingCache: true,
+        };
+        modernScreenshot.domToPng(document.querySelector(`#${containerId}`), {
+            fetch: fetchOptions,
+            filter: filterFn,
+            scale: 2,
+            quality: 0.95,
+        }).then(dataUrl => {
+            const link = document.createElement('a');
+            link.download = `${currentPersonalityType.name}(${currentPersonalityType.type})-${Date.now()}.png`;
+            link.href = dataUrl;
+            link.click();
+        })
+    }
+    const calculatePersonalityType = (dimensions) => {
+        const type = dimensions
+            .slice(0, 4)
+            .map(d => d.id[getDominantTraitIndex(d.value[1])].toUpperCase())
+            .join('');
+        const personalityType = personalityTypes.find(p => p.type === type);
+
+        if (!personalityType) {
+            return { type, name: '未知类型', desc: '' };
+        }
+
+        let result = {
+            type,
+            name: getLocalizedContent(personalityType.name),
+            desc: getLocalizedContent(personalityType.desc),
+        };
+        result = {
+            ...result,
+            avatar: `${imagesHostUrl}/${result.type.toLowerCase()}-${personalityType.name.en.toLowerCase()}-s3-v1-${config.gender}.png?t=${Date.now()}`,
+            // avatar: personalityType.avatar[config.gender]
+        }
+
+        if (dimensions.length > 4) {
+            const atDimension = dimensions[4].value[0] < 50 ? 'A' : 'T';
+            result.type += '-' + atDimension;
+        }
+        return result;
+    };
+
+    const containerId = `mbti-${config.cardType}-container`;
+    let mbtiDimensions = getMBTIDimensions(config);
+    const baseMBTIDimensions = JSON.parse(JSON.stringify(mbtiDimensions));
+    const container = document.getElementById(containerId);
+    container.setAttribute('lang', config.language);
+    let currentPersonalityType = calculatePersonalityType(mbtiDimensions);
+    const basePersonalityType = JSON.parse(JSON.stringify(currentPersonalityType));
+    container.innerHTML = createHeaderHTML(currentPersonalityType) +
+        getMBTIBars(mbtiDimensions) +
+        createFooterHTML(currentPersonalityType);
+
+    initializeSliders(mbtiDimensions, config.slide);
+    if (config.slide) {
+        document.getElementById('reset-mbti').addEventListener('click', resetMBTI);
+    }
+    document.getElementById("download-mbti").addEventListener("click", generatePersonalityImage);
+    const throttledUpdateAllTooltipPositions = throttle(updateAllTooltipPositions, 100);
+    window.addEventListener('resize', throttledUpdateAllTooltipPositions);
+
+
+}
+
+function debounce(func, delay) {
+    let timer;
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(() => func.apply(this, args), delay);
+    };
+}
+
+function throttle(func, limit) {
+    let inThrottle;
+    return function(...args) {
+        if (!inThrottle) {
+            func.apply(this, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
